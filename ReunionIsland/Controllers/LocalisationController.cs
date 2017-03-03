25eb25a24027970008e7d2a171669463935cf870
pierakim <pierakim.lambda@gmail.com>
2017-03-03 19:50:58 +1000

    Localisation
    Add localisation controllers/Views + gmap localisation

---
+++ ReunionIsland/Controllers/LocalisationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReunionIsland.Web.Controllers
+{
+    public class LocalisationController : Controller
+    {
+        // GET: Localisation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
